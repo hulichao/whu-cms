de0358f8c3ef9216cf80bece869b43e77979e057
hulichao <jsu_hlc@163.com>
2017-11-29 12:00:00 +0800

    修改 CmsSessionListener

--- whu-web/src/main/java/tech/whu/cms/web/CmsSessionListener.java
+++ whu-web/src/main/java/tech/whu/cms/web/CmsSessionListener.java
@@ -12,6 +12,11 @@ public class CmsSessionListener implements HttpSessionListener {
 	@Override
 	public void sessionDestroyed(HttpSessionEvent event) {
 		CmsSessionContext.removeSession(event.getSession());
+
+
+
+
+
 		System.out.println("移除了Session:"+event.getSession().getId());
 	}
 
