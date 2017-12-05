c04a207d714c9ebc9a9b40b49e78702efa2bdc8f
hulichao <jsu_hlc@163.com>
2017-12-05 12:00:00 +0800

    修改 UserController

--- whu-web/src/main/java/tech/whu/cms/controller/UserController.java
+++ whu-web/src/main/java/tech/whu/cms/controller/UserController.java
@@ -9,6 +9,8 @@ import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
 
 import org.springframework.stereotype.Controller;
+
+
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PathVariable;
