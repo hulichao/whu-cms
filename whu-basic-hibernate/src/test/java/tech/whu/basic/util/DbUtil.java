2a7769b8328c28e5e1c423f2775cc770fd618988
hulichao <jsu_hlc@163.com>
2018-01-16 12:00:00 +0800

    修改 DbUtil

--- whu-basic-hibernate/src/test/java/tech/whu/basic/util/DbUtil.java
+++ whu-basic-hibernate/src/test/java/tech/whu/basic/util/DbUtil.java
@@ -8,6 +8,7 @@ import java.sql.SQLException;
 
 public class DbUtil {
 	public static Connection getConnection() throws SQLException {
+
 		Connection con = null;
 		con = DriverManager.getConnection("jdbc:mysql://localhost:3306/wetech_cms",
 				"root", "123456");
